7627d0e4583f75a34abd727ffb4514e08103450e
jsshin <tb25271@gmail.com>
2025-02-27 17:48:43 +0900

    [feat] backend 업데이트

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClsModule } from 'nestjs-cls';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { PrismaModule, PrismaService } from 'nestjs-prisma';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 
 @Module({
-  imports: [],
+  imports: [
+    ClsModule.forRoot({
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [PrismaModule],
+          adapter: new TransactionalAdapterPrisma({
+            prismaInjectionToken: PrismaService,
+          }),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/common/utils/data.helper.ts
+++ backend/src/common/utils/data.helper.ts
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 
 export class DataValidation {
+  public static isNotEmpty(value: any): boolean {
+    return !_.isEmpty(value);
+  }
+
   public static isEmpty(value: any): boolean {
     return _.isEmpty(value);
   }
